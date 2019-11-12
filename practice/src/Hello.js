2c4115848725a0959e38d9f9a8e11ab26023c50d
Carl-Stanley <cds.beml@gmail.com>
2019-11-12 10:49:15 -0700

    COmmit

--- practice/src/Hello.js
+++ practice/src/Hello.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Hello = () => {
-    return (
-    <div><h1>Hello!</h1></div>
-    )
+  //  return (
+  //  <div><h1>Hello!</h1></div>
+  //  )
+    return React.createElement('div',{id: 'Hello', class: 'dummyClass'},React.createElement('h1',null,'Hello from create element!'));
+
+
 }
+// Rewitten in JSX ^
+
 
 export default Hello;
 
