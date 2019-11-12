fba2fd51b5b2f9874270a92886e4f1c46df0e534
Carl-Stanley <cds.beml@gmail.com>
2019-11-12 09:27:26 -0700

    Commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Example from './Example';
 class App extends Component {
   
 
   render () {
+    
     <div className="App">
       
+      <Example />
        
     </div>
   };
